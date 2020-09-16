85712de4a581fc18a1b63e94a8fd0fe48d70d4b9
damien <damien@thoorens.fr>
2020-09-16 22:27:17 +0200

    rename ajaxCheck.php

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -39,7 +39,7 @@ $(function () {
 		.bind("keyup change blur input mouseenter", throttle(function () {
 			let pseudo = $(this).val().trim();
 			if (pseudo != '') {
-				$.get('controllers/ajaxCheck.php', {
+				$.get('controllers/ajaxUserCheck.php', {
 						pseudo: pseudo
 					})
 					.done(function (data) {
@@ -53,7 +53,7 @@ $(function () {
 		.bind("keyup change blur input mouseenter", throttle(function () {
 			let email = $(this).val().trim();
 			if (email != '') {
-				$.get('controllers/ajaxCheck.php', {
+				$.get('controllers/ajaxUserCheck.php', {
 						email: email
 					})
 					.done(function (data) {
